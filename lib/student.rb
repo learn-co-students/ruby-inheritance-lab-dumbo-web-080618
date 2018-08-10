48e61325cacdbc1ba7a9b491e0730de1b62a0ac5
github username <github email address>
2018-08-10 14:40:39 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,14 @@
 require_relative "user.rb"
+require 'pry'
 class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    super
+    @knowledge = []
+  end
+
+  def learn(know_how)
+    @knowledge << know_how
+  end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,4 @@
-class User
-  :attr_accessor :first_name, :last_name
 
-  def initialize (first, last)
-    @first_name = first
-    @last_name = last
-  end
+class User
+  attr_accessor :first_name, :last_name
 end
